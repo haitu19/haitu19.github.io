75fdae8d4092ae54a407c865c164aeb858d66250
Hai <haitu19pt@gmail.com>
2019-12-12 16:20:03 +0700

    up sango

---
+++ sango/js/main.js
@@ -0,0 +1,83 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    //toggle-menu
+    $(".category-name").on('click', function () {
+        $(".menu-toggle").toggleClass("on");
+        $(".category-list").toggleClass("active");
+        $(".menu-toggle-mobile").removeClass("on");
+        $("ul.nav").removeClass("active");
+        $(".bg-black-menu").removeClass("active");
+        $(".bg-black-category").toggleClass("active");
+    });
+    $(".menu-toggle").on('click', function () {
+        $(".menu-toggle").toggleClass("on");
+        $(".category-list").toggleClass("active");
+        $(".menu-toggle-mobile").removeClass("on");
+        $("ul.nav").removeClass("active");
+        $(".bg-black-menu").removeClass("active");
+        $(".bg-black-category").toggleClass("active");
+    });
+
+    $(".category-name").on('click', function () {
+        $(".category-list-mobile").toggleClass("active");
+    });
+    $(".menu-toggle").on('click', function () {
+        $(".category-list-mobile").toggleClass("active");
+    });
+
+    $('.bg-black-menu').click(function () {
+        $('.menu-toggle-mobile').removeClass('on');
+        $("ul.nav").removeClass("active");
+        $(this).removeClass('active');
+    })
+    $('.bg-black-category').click(function () {
+        $('.menu-toggle').removeClass('on');
+        $(".category-list-mobile").removeClass("active");
+        $(".category-list").removeClass("active");
+        $(this).removeClass('active');
+    })
+
+    $(".category-name-mobile").on('click', function () {
+        $(".menu-toggle-mobile").toggleClass("on");
+        $("ul.nav").toggleClass("active");
+        $(".bg-black-menu").toggleClass("active");
+        $(".menu-toggle").removeClass("on");
+        $(".category-list-mobile").removeClass("active");
+        $(".category-list").removeClass("active");
+        $(".bg-black-category").removeClass("active");
+    });
+    $(".menu-toggle-mobile").on('click', function () {
+        $(".menu-toggle-mobile").toggleClass("on");
+        $("ul.nav").toggleClass("active");
+        $(".menu-toggle").removeClass("on");
+        $(".bg-black-menu").toggleClass("active");
+        $(".category-list-mobile").removeClass("active");
+        $(".category-list").removeClass("active");
+        $(".bg-black-category").removeClass("active");
+    });
+
+    //slick
+    $('.slick-slider').slick();
+
+    //video iframe
+    $('.video-small .video').click(function (e) {
+        e.preventDefault();
+        var sr = $(this).attr('data-src');
+        var name = $(this).attr('data-name');
+        $('.vdbig').attr('src', sr);
+        $('.video-big .title').text(name);
+    })
+    //scroll up
+    $(document).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollup').fadeIn('slow');
+            $('.scrollup').removeClass('hideScr');
+        }
+        else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 1000);
+    });
+})
---
+++ sango/js/social.js
@@ -0,0 +1,72 @@
+﻿$(function () {
+    $(".post-size .large").click(function (e) {
+        e.preventDefault();
+        $(".post-content").each(function () {
+            var size = parseInt($(this).css("font-size"));
+            var lineheight = parseInt($(this).css("line-height"));
+            size = size + 1 + "px";
+            lineheight = lineheight + 2 + "px";
+            $(this).css({
+                'font-size': size,
+                'line-height': lineheight
+            });
+            $(this).find("*").css({
+                'font-size': size,
+                'line-height': lineheight
+            });
+        });
+    });
+    $(".post-size .small").click(function (e) {
+        e.preventDefault();
+        $(".post-content").each(function () {
+            var size = parseInt($(this).css("font-size"));
+            var lineheight = parseInt($(this).css("line-height"));
+            size = size - 1 + "px";
+            lineheight = lineheight - 2 + "px";
+            $(this).css({
+                'font-size': size,
+                'line-height': lineheight
+            });
+            $(this).find("*").css({
+                'font-size': size,
+                'line-height': lineheight
+            });
+        });
+    });
+    $(".post-size .normal").click(function (e) {
+        e.preventDefault();
+        $(".post-content").each(function () {
+            var size = parseInt($(this).css("font-size"));
+            var lineheight = parseInt($(this).css("line-height"));
+            size = 15 + "px";
+            lineheight = 24 + "px";
+            $(this).css({
+                'font-size': size,
+                'line-height': lineheight
+            });
+            $(this).find("*").css({
+                'font-size': size,
+                'line-height': lineheight
+            });
+        });
+    });
+});
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src =
+      'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.11&appId=453642988336652';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+(function () {
+    var po = document.createElement("script");
+    po.type = "text/javascript";
+    po.async = true;
+    po.src = "https://apis.google.com/js/platform.js";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(po, s);
+})();
